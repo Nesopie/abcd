d85c35728969a3186e5ad87037102b7f5d34820c
Nesopie <87437291+Nesopie@users.noreply.github.com>
2023-11-26 09:37:33 +0530

    Update d.ts

--- d/src/lib/d.ts
+++ d/src/lib/d.ts
@@ -4,5 +4,5 @@ import { c } from '@abcdpackage/c';
 import { MaxUint256 } from 'ethers';
 
 export function d(): string {
-  return a() + b() + c() + 'd' + MaxUint256;
+  return a() + b() + c() + 'd';
 }
